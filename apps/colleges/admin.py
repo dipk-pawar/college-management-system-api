109ccf4d3471f10c3bb16d4f5799965f0f076ad9
Dipak Pawar <pawardipak482@gmail.com>
2023-08-01 14:27:39 +0530

    initialized the project and added the models

---
+++ apps/colleges/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import College, Role, Course
+
+# Register your models here.
+admin.site.register(College)
+admin.site.register(Role)
+admin.site.register(Course)
---
+++ apps/colleges/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+class College(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    established_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    college = models.ForeignKey("colleges.college", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    college = models.ForeignKey("colleges.college", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
