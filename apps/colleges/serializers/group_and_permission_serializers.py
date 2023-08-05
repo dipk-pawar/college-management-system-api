cf3945b451e38b01ce249fc16cc0c5714c53005d
Dipak Pawar <pawardipak482@gmail.com>
2023-08-05 18:49:45 +0530

    added permissions in college group

--- apps/colleges/serializers/group_and_permission_serializers.py
+++ apps/colleges/serializers/group_and_permission_serializers.py
@@ -9,28 +9,39 @@ from django.db import IntegrityError
 class CollegeGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = CollegeGroup
-        fields = ["id", "group_name"]
+        fields = ["id", "group_name", "permissions"]
         extra_kwargs = {
             "id": {"read_only": True},
+            "group_name": {"required": True},
+            "permissions": {"required": True},
         }
 
     def validate(self, attrs):
         group_name = attrs.get("group_name")
+        permissions = attrs.get("permissions")
         if len(group_name) <= 2:
             raise serializers.ValidationError(
                 {"message": "Length of group name must be Greater then 2"}
             )
+        if len(permissions) == 0:
+            raise serializers.ValidationError(
+                {"message": "Please assign atlist 1 permission"}
+            )
+
         return attrs
 
     def create(self, validated_data):
         request = self.context.get("request")
         user = request.user if request else None
         group_name = validated_data["group_name"]
+        permissions = validated_data["permissions"]
         name = GenerateRandomChar.generate_username(group_name)
         try:
-            return CollegeGroup.objects.create(
+            college_group = CollegeGroup.objects.create(
                 name=name, group_name=group_name, college_id=user.college.id
             )
+            college_group.permissions.set(permissions)
+            return college_group
         except IntegrityError as e:
             raise serializers.ValidationError(
                 {"message": "Group name already exists for this college"}
@@ -38,13 +49,13 @@ class CollegeGroupSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         group_data = super().to_representation(instance)
-        group_data["permissions"] = Group.objects.filter(
-            permissions__in=instance.permissions.all()
+        group_data["permissions"] = Permission.objects.filter(
+            id__in=group_data["permissions"]
         ).values(
-            "permissions__id",
-            "permissions__codename",
-            "permissions__name",
-            "permissions__content_type_id",
+            "id",
+            "codename",
+            "name",
+            "content_type_id",
         )
         return group_data
 
