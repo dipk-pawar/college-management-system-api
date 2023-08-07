4330e7a32ddf3cf63ddffdb9a131f417e40779ec
Dipak Pawar <pawardipak482@gmail.com>
2023-08-07 22:54:55 +0530

    changes in registration API

--- apps/accounts/serializers/user_serializer.py
+++ apps/accounts/serializers/user_serializer.py
@@ -10,7 +10,7 @@ from apps.colleges.serializers.college_serializers import (
 )
 from apps.common.helpers.custom_exeception_helper import ExceptionError
 from apps.common.helpers.user_helper import GenerateRandomChar
-from apps.colleges.models import Role
+from apps.colleges.models import Course, Role
 
 
 def validate_email_address(email):
@@ -76,49 +76,51 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff": {"read_only": True},
             "is_admin": {"read_only": True},
             "is_superuser": {"read_only": True},
-            "college": {"required": True},
-            "role": {"required": True},
-            "courses": {"required": True},
         }
 
-    def to_internal_value(self, data):
-        role_id = data.get("role")
-        if role_id is not None:
-            request_user = self.context.get("request").user
-            roles = list(
-                Role.objects.filter(college_id=request_user.college.id).values_list(
-                    "id", flat=True
-                )
+    def is_valid(self, raise_exception=False):
+        request_user = self.context.get("request").user
+        role_id = self.initial_data.get("role")
+        roles = list(
+            Role.objects.filter(college_id=request_user.college_id).values_list(
+                "id", flat=True
             )
-            if role_id not in roles:
-                raise serializers.ValidationError(
-                    {
-                        "role": "Sorry, role not found. Create a role before assigning it."
-                    }
-                )
-
-        return super().to_internal_value(data)
+        )
+        if role_id not in roles:
+            raise ExceptionError(
+                "Sorry, role not found. Create a role before assigning it."
+            )
+        courses = self.initial_data.get("courses")
+        college_courses = list(
+            Course.objects.filter(college_id=request_user.college_id).values_list(
+                "id", flat=True
+            )
+        )
+        for course in courses:
+            if course not in college_courses:
+                raise ExceptionError("Sorry, course not found.")
+        return super(UserSerializer, self).is_valid(raise_exception=raise_exception)
 
     def get_college(self, obj):
         request = self.context.get("request")
         return CollegeSerializer(request.user.college).data if request else None
 
-    def create(self, validated_data):
-        request = self.context.get("request")
-        courses = validated_data.get("courses", None)
-        validated_data["college"] = request.user.college
-        validated_data.pop("courses", None)
-        validated_data["password"] = GenerateRandomChar.generate_password(12)
-        user = User.objects.create_user(**validated_data)
-        if courses:
-            user.courses.set(courses)
-        return user
+    # def create(self, validated_data):
+    #     request = self.context.get("request")
+    #     courses = validated_data.get("courses", None)
+    #     validated_data["college"] = request.user.college
+    #     validated_data.pop("courses", None)
+    #     validated_data["password"] = GenerateRandomChar.generate_password(12)
+    #     user = User.objects.create_user(**validated_data)
+    #     if courses:
+    #         user.courses.set(courses)
+    #     return user
 
-    def to_representation(self, instance):
-        user_data = super().to_representation(instance)
-        user_data["courses"] = CourseSerializer(instance.courses.all(), many=True).data
-        user_data["role"] = (
-            RoleSerializer(instance.role).data if instance.role else None
-        )
-        user_data["role"]["college"] = instance.college.id
-        return user_data
+    # def to_representation(self, instance):
+    #     user_data = super().to_representation(instance)
+    #     user_data["courses"] = CourseSerializer(instance.courses.all(), many=True).data
+    #     user_data["role"] = (
+    #         RoleSerializer(instance.role).data if instance.role else None
+    #     )
+    #     user_data["role"]["college"] = instance.college.id
+    #     return user_data
--- apps/accounts/views/user_views.py
+++ apps/accounts/views/user_views.py
@@ -3,11 +3,13 @@ from rest_framework.views import APIView
 from rest_framework import status, generics
 from apps.accounts.serializers.user_serializer import LoginSerializer, UserSerializer
 from apps.common.error_helper import FormatError
+from apps.common.helpers.user_helper import GenerateRandomChar
 from cms.jwt_custom_token import get_tokens_for_user
 from django.db import transaction
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from apps.common.permissions import SuperUserORAdmin
 from apps.common.helpers.error_decorator import track_error
+from django.contrib.auth.hashers import make_password
 
 
 # Create your views here.
@@ -36,27 +38,31 @@ class RegisterUser(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated, SuperUserORAdmin)
 
+    @track_error(
+        validate_api_parameters=["email", "first_name", "last_name", "role", "courses"]
+    )
     @transaction.atomic
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        print(request.data)
-        if not serializer.is_valid():
-            return Response(
-                {
-                    "error": True,
-                    "message": FormatError.format_serializer_errors(
-                        errors=serializer.errors
-                    ),
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        serializer.save()
+        serializer.is_valid(raise_exception=True)
+        password = GenerateRandomChar.generate_password(12)
+        user = serializer.save(
+            college_id=request.user.college_id, password=make_password(password)
+        )
+        user.courses.set(request.data.get("courses"))
 
         return Response(
             status=status.HTTP_201_CREATED,
             data={
                 "error": False,
-                "data": serializer.data,
+                "data": {
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "email": user.email,
+                    "password": password,
+                    "college": user.college.name,
+                    "role": user.role.name,
+                },
                 "message": "User created successfully",
             },
         )
